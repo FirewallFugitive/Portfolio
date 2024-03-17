4d90043e307134d42bc7a054813b76bee0b526ae
XiriusBlack <74360576+XiriusBlack@users.noreply.github.com>
2024-03-17 15:14:34 +0100

    constante, template literals, destructuring
    eerste versie van deze dingen toegevoegd aan portfolio

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 'use strict'
 
+const creater = "Lucas Moons";
+console.log(`${creater} = de creater van dit portfolio`);
 let tasks = [];
 let button = document.getElementById("taskbtn")
 let btn2 = document.getElementById("btn2")
+let name;
+let categorie;
 button.addEventListener('click',function(){
-    let task = document.getElementById("taskInput").value 
+    let task = {
+        name: document.getElementById("taskInput").value,
+        categorie: document.getElementById("catInput").value
+    }
+    let {name,categorie} = task;
     tasks.push(task)
     let p = document.createElement("p")
-    p.innerHTML = task
+    p.innerHTML = `- ${task.name} Categorie: ${task.categorie}`
     document.getElementById("tasksDiv").appendChild(p)
 })
 btn2.addEventListener('click',function(){
     console.log(tasks)
 })
+
