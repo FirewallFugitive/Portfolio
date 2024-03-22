f96306c34219407dde482451409c30a17a832696
XiriusBlack <74360576+XiriusBlack@users.noreply.github.com>
2024-03-23 00:29:01 +0100

    Form validation, arrow fucntion
    formulier validatie toegevoegd en functie van createTask hieraan gekoppeld. Ook input verandert zodat er 2 categorieen zijn en ook date de duration van een task kan worden meegegeven. Arrow function toegepast op functie bij berekening totale duration

--- script.js
+++ script.js
@@ -4,21 +4,44 @@ const creater = "Lucas Moons";
 console.log(`${creater} = de creater van dit portfolio`);
 let tasks = [];
 let button = document.getElementById("taskbtn")
-let btn2 = document.getElementById("btn2")
 let name;
 let categorie;
-button.addEventListener('click',function(){
-    let task = {
-        name: document.getElementById("taskInput").value,
-        categorie: document.getElementById("catInput").value
-    }
-    let {name,categorie} = task;
-    tasks.push(task)
-    let p = document.createElement("p")
-    p.innerHTML = `- ${task.name} Categorie: ${task.categorie}`
-    document.getElementById("tasksDiv").appendChild(p)
-})
+let duration;
 btn2.addEventListener('click',function(){
-    console.log(...tasks)
+    console.log(...tasks);
+    totalDuration();
 })
+function validateForm() {
+    let x = document.forms["taskForm"]["taskInput"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+    else{
+        let task = {
+            name: document.getElementById("taskInput").value,
+            categorie: document.querySelector("input[name='taskType']:checked").value,
+            duration: document.getElementById("hourInput").value + ":" +document.getElementById("minInput").value
+        }
+        let {name,categorie,duration} = task;
+        tasks.push(task)
+        let p = document.createElement("p")
+        p.innerHTML = `- ${task.name} Categorie: ${task.categorie} Duration ${task.duration}`
+        document.getElementById("tasksDiv").appendChild(p)
+    }
+  }
 
+let Duration = (h,m) => {
+    let result = (h*60)+m;
+    return result;
+}
+
+function totalDuration(){
+    let total = 0;
+    for(let i = 0; i < tasks.length; i++){
+        let time = tasks[i].duration; 
+        const timeArray = time.split(":");
+        total += Duration(parseInt(timeArray[0]), parseInt(timeArray[1]));
+    }
+    console.log(total);
+}
